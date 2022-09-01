c553e488bb05ce04180a0478e94b154db5f7364c
AGlanborgBTH <93671007+AGlanborgBTH@users.noreply.github.com>
2022-09-01 19:56:34 +0200

    v1.0.0
    First draft

---
+++ src/App.svelte
@@ -0,0 +1,53 @@
+<script>
+  import "trix/dist/trix.js";
+  import "trix/dist/trix.css"
+
+  let content
+
+  function handleSave () {
+    console.log(content.outerText)
+  }
+
+</script>
+
+<main>
+  <form on:submit|preventDefault={() =>{}}>
+    <div class="saveContainer">
+      <input on:click={handleSave} type="button" value="Save Project" class="save">
+    </div>
+    <input id="x" type="hidden" name="content">
+    <trix-editor bind:this={content} class="trix-content" input="x"></trix-editor>
+  </form>
+</main>
+
+<style>
+  main {
+    height: 90vh;
+    width: 1280px;
+    background-color: rgb(220, 220, 220);
+    color: rgb(0, 0, 0);
+  }
+
+  form {
+    height: 100%;
+    width: 100%;
+    overflow-y: hidden;
+  }
+
+  trix-editor {
+    display: inherit;
+    height: 100%;
+    text-align: left;
+  }
+
+  .saveContainer {
+    display: flex;
+    flex-direction: row;
+    background-color: rgb(60, 60, 60);
+  }
+
+  .save {
+    padding: 5px;
+    margin: 10px 20px;
+  }
+</style>
---
+++ src/lib/trix.svelte
@@ -0,0 +1,21 @@
+<script>
+  import "trix/dist/trix";
+</script>
+
+<div>
+  <input id="x" type="hidden" name="content" />
+  <trix-editor class="trix-content" input="x" />
+</div>
+
+<style>
+  trix-editor {
+    display: inherit;
+    height: 90%;
+    text-align: left;
+  }
+
+  div {
+    height: 100%;
+    width: 100%;
+  }
+</style>
